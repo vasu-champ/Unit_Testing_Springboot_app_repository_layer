92f78763f654e3e4197b57d47e326bd6ca7e205c
vasu-champ <vasu.walk@gmail.com>
2023-09-12 18:33:10 +0530

    Unit Testing Spring Data Jpa Repository Layer

---
+++ spring-boot-repository-test/src/main/java/com/vasu/springbootrepositorytest/SpringBootRepositoryTestApplication.java
@@ -0,0 +1,13 @@
+package com.vasu.springbootrepositorytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRepositoryTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRepositoryTestApplication.class, args);
+	}
+
+}
---
+++ spring-boot-repository-test/src/test/java/com/vasu/springbootrepositorytest/SpringBootRepositoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vasu.springbootrepositorytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRepositoryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
